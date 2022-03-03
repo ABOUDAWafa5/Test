7ffbf10be9185632834ea37ab45e402bf0812728
aboudawafa5 <aboudawafa02@gmail.com>
2022-03-03 21:14:20 +0100

    first commit

---
+++ resources/users/index.blade.php
@@ -0,0 +1,60 @@
+
+
+@section('title', 'Users List')
+
+@section('content')
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Users</h1>
+            <div class="row">
+            
+            
+                
+            </div>
+
+        </div>
+
+    
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">All Users</h6>
+
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th width="20%">Name</th>
+                                <th width="25%">Email</th>
+                           
+                          
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    {{ $users->links() }}
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+@endsection
+
+
+@endsection
